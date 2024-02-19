1957f308a2c2c345d0b0a2089eaec50e73106c11
Edison <edisonm31@yahoo.ca>
2024-02-19 12:32:49 -0400

    changes to dynamic weather

---
+++ main.js
@@ -0,0 +1,11 @@
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/CAR/73,64/forecast")
+  const weatherData = await weatherPromise.json()
+
+  const ourTemperature = weatherData.properties.periods[0].temperature
+
+  document.querySelector("#temperature-output").textContent = ourTemperature
+
+}
+
+start()
