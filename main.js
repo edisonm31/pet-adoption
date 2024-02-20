8a25d9ba21971594d5bfd2820b400e6afbd3a5aa
Edison <edisonm31@yahoo.ca>
2024-02-20 17:13:05 -0400

    finished pet filtering

--- main.js
+++ main.js
@@ -19,11 +19,13 @@ async function petsArea() {
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
 
+    clone.querySelector(".pet-card").dataset.species = pet.species
+
     clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
 
-    if (!pet.photo) pet.photo = "images/fallback.jpeg"
+    if (!pet.photo) pet.photo = "images/fallback.jpg"
     clone.querySelector(".pet-card-photo img").src = pet.photo
     clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
 
@@ -44,4 +46,29 @@ function createAgeText(birthYear) {
   if (age == 0) return "Less than a year old"
 
   return ` ${age} years old`
+}
+
+// pet filter code
+
+const allButtons = document.querySelectorAll(".pet-filter button")
+allButtons.forEach(el => {
+  el.addEventListener("click", handleButtonClick)
+})
+
+function handleButtonClick(e) {
+  //remove active class from any and all buttons
+  allButtons.forEach(el => el.classList.remove("active"))
+
+  //add active class to the specific button that just got clicked
+  e.target.classList.add("active")
+
+  //actually filter the pets down below
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid"
+    } else {
+      el.style.display = "none"
+    }
+  })
 }
